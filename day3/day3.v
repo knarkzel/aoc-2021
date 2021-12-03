d9bdd0ba9e477a5b2e108827b5260e2adb394c3e
Knarkzel <knarkzel@gmail.com>
2021-12-03 23:10:00 +0100

    latest

--- day3/day3.v
+++ day3/day3.v
@@ -3,6 +3,11 @@ import math
 
 fn main() {
 	mut bytes := os.read_lines('input.txt') ?
+	println('Part 1: ${part1(bytes)}')
+	println('Part 2: ${part2(bytes)}')
+}
+
+fn get_bitmap(bytes []string) []bool {
 	rows := bytes.len
 	columns := bytes[0].len
 	mut common := map[int]int{}
@@ -17,11 +22,12 @@ fn main() {
 	for i in 0 .. columns {
 		bitmap << common[i] >= rows / 2
 	}
-	println('Part 1: ${part1(bitmap, columns)}')
-	println('Part 2: ${part2(bytes, bitmap)}')
+	return bitmap
 }
 
-fn part1(bitmap []bool, columns int) int {
+fn part1(bytes []string) int {
+	columns := bytes[0].len
+	bitmap := get_bitmap(bytes)
 	mut gamma_rate, mut epsilon_rate := 0, 0
 	for i, bit in bitmap {
 		if bit {
@@ -33,10 +39,20 @@ fn part1(bitmap []bool, columns int) int {
 	return gamma_rate * epsilon_rate
 }
 
-fn part2(bytes []string, bitmap []bool) []string {
+fn part2(bytes []string) string {
 	mut rest := bytes.clone()
-	for i, bit in bitmap {
-		rest = rest.filter(it[i] == if bit { `1` } else { `0` })
+	for rest.len > 1 {
+		bitmap := get_bitmap(rest)
+		for i, bit in bitmap {
+			// amount with 1 in pos
+			ones := rest.map(it[i] == `1`).len
+			zeros := rest.map(it[i] == `0`).len
+			if ones == zeros {
+				// oxygen generator rating
+			} else {
+				rest = rest.filter(it[i] == if bit { `1` } else { `0` })
+			}
+		}
 	}
-	return rest
+	return rest[0]
 }
